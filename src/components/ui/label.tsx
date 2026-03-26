a53261d8fe777440011dc793221eeb5b49d4d38d
frankalvarezdev <64846247+frankalvarezdev@users.noreply.github.com>
2026-03-26 10:12:10 -0500

    fix: se elimino el uso de '@base-ui/react/label'

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-import { Label as LabelPrimitive } from '@base-ui/react/label'
 import type * as React from 'react'
 
 import { cn } from '@/lib/utils'
 
 function Label({ className, ...props }: React.ComponentProps<'label'>) {
 	return (
-		<LabelPrimitive
+		// biome-ignore lint/a11y/noLabelWithoutControl: Is necessary
+		<label
 			data-slot='label'
 			className={cn(
 				'flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50',
