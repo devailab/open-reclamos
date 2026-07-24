037255e17b1795a45b3a71978cc12009b804c178
frankalvarezdev <64846247+frankalvarezdev@users.noreply.github.com>
2026-07-23 21:53:13 -0500

    feat: init docs

---
+++ docs/src/lib/shared.ts
@@ -0,0 +1,10 @@
+export const appName = 'Open Reclamos'
+export const docsRoute = '/docs'
+export const docsImageRoute = '/og/docs'
+export const docsContentRoute = '/llms.mdx/docs'
+
+export const gitConfig = {
+	user: 'devailab',
+	repo: 'open-reclamos',
+	branch: 'main',
+}
